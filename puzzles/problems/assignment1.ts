7a016b1b09bc91a152107cd26d03256a07b749d3
Jared <jared.s.violo@gmail.com>
2025-10-13 11:59:47 -0400

    done

--- puzzles/problems/assignment1.ts
+++ puzzles/problems/assignment1.ts
@@ -14,6 +14,35 @@
  * - [{ name: "Charlie", scores: [80] }, { name: "Dana", scores: [80, 80] }] → "Charlie"
  */
 
-export function findTopStudent(students): string {
-  return "TODO"
+interface student {
+  name: string;
+  scores: number[];
+}
+
+export function findTopStudent(students: student[]): string {
+  let averageScores = [];
+
+  for (let student = 0; student < students.length; student++) {
+    // for each student
+    let Totalscores = 0;
+
+    for (let s = 0; s < students[student].scores.length; s++) {
+      // for each grade in student
+      Totalscores += students[student].scores[s];
+    }
+
+    const average = Totalscores / students[student].scores.length;
+    const name = students[student].name;
+
+    averageScores.push({ name, average });
+  }
+
+  averageScores.sort((a, b) => a.average - b.average);
+  console.log("students" + JSON.stringify(averageScores));
+
+  if (averageScores.length === 0) {
+    return "No students provided";
+  } else {
+    return averageScores[averageScores.length - 1].name;
+  }
 }
